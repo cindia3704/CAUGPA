0472ff263eddd5d5dccb49883213c470486c94a5
cindia3704 <cindia3704@gmail.com>
2021-12-07 23:20:48 +0900

    add initial files

---
+++ CAUGPA/app/src/main/java/com/example/caugpa/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.caugpa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.caugpa.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+//    static final int SERVICE_ID = 1001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+
+//    public void buttonClick(View view){
+//        Intent intent = new Intent(this, MyService.class);
+//        Toast.makeText(this,"hi",Toast.LENGTH_LONG).show();
+//        startService(intent);
+//    }
+}
